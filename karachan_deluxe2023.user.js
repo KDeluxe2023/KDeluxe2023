ad039b4bc9755e9e601d170396c72d8d16174ecf
KDeluxe2023 <119752397+KDeluxe2023@users.noreply.github.com>
2022-12-03 18:09:47 +0100

    Update karachan_deluxe2023.user.js

--- karachan_deluxe2023.user.js
+++ karachan_deluxe2023.user.js
@@ -14,44 +14,9 @@
 // @license      MIT
 // @grant        none
 // @require      https://code.responsivevoice.org/responsivevoice.js
-// @updateURL https://greasyfork.org/scripts/455931-karachan-deluxe-2023/code/Karachan%20Deluxe%202023.user.js
-// @downloadURL https://greasyfork.org/scripts/455931-karachan-deluxe-2023/code/Karachan%20Deluxe%202023.user.js
+// @updateURL https://github.com/KDeluxe2023/KDeluxe2023/raw/main/karachan_deluxe2023.user.js
+// @downloadURL https://github.com/KDeluxe2023/KDeluxe2023/raw/main/karachan_deluxe2023.user.js
 // ==/UserScript==
-
-/*
-TO-DO:
->opcje robienia nicków, w sensie każdy anon sobie wpisuje nicka i można potem kogoś pingować np. @cieć_inwalida i wtedy u tego ciecia inwalidy z rozjebaną ręką pojawi się dźwięk powiadomienia jak z GG który będzie dzialał na podstawie search.php mitsuby
->dumper z opcją pobierania obrazków oraz z opcją pominięcia ukrytych postujących dobry pomysł
->poprawienie pobierania plików żeby nie pobrać thumbnaila albo dodanie jakiegoś downloader żeby pobrać całą nitkę lub bobrazki z nitki
->dodanie zmieniarki hasła po tym jak dostanie ono bana na ryj
->pokazywanie czy masz bana lub nie
->normalizacja poziomu głośności w sensie anty-krzykacz
->Spoilerowanie obrazka na stałe (po hashu)
->krawężnik na postującego (ukryj skurwysyna) w oparciu na mutationobserver (z filtrami)
->autokrawężnik z filtrami (nazwa obrazka/substring we fredzie)
->statystyki: ile tematów skrawężnikowałeś, ile postów napisałeś, ile metrów przejechałeś myszką, ile czasu spędziłeś na czanie
->ulepszone tytuły stron
->przyciski do przeglądania poprzedniego/następnego posta z tym samym ID postującego
->content freeze (usunięte posty są oznaczane jako usunięte i nie znikają po odświezeniu strony (trzeba mieć otwartego freda))
->uproszczone sagowanie
->przepisany, odbugowany fastreply
->https://greasyfork.org/en/scripts/26707-kurahen-idhighlight/code
->https://github.com/Kurahen-Premium/Kurahen-Premium
->https://greasyfork.org/en/scripts/370095-konfident/code
->pełna lista wordfiltrów w kreatorze posta
->zamienianie słowofiltrów w polu treści posta w czasie rzeczywistym
->anti-ban (nie pozwala zapostować jeśli dostałbyś auto-bana) - przydatne jak np. wstawiam kod albo kopiujemy skądś tekst gdzie są emotki
->zmiana tytułu deski
->bugfix na listę obserwowanych fredów poza ekranem
->ukrywanie boarda /noc/ kiedy nie jest dostępny
->krawężnik w katalogu
->dyskretny guzik przy kazdym obrazku gdy jest odpalony w scrollowanym powiększaczo-viewerze, guzik ktory nada styl typu transform invert 180deg zeby biale obraski nie jebaly po oczach (el.style['filter'] = el.style['filter'] === '' ? 'hue-rotate(180deg) invert(1)' : ''; )
->dodaj tzw. sikret STAROCIOTY PAMIĘTAJĄ anony bez skryptu widzą tylko ciąg losowych słów i znaków, a ci ze skryptem pasek w kolorze picrel i żeby rozkodować trzeba podać hasło https://pastebin.com/Qv1pd284
->ukrywanie postow <x odpowiedzi x do wyboru samemu
->biegający bałwanek u dołu ekranu, wybór gifa i prędkość i coś jeszcze było by git
->opcja pisania tekstem zalgo
->trójsiła: https://software.hixie.ch/utilities/cgi/unicode-decoder/character-identifier?characters=%C2%A0%C2%A0%E2%96%B2+
-*/
 
 ////// shared functions
 const log = function(m) { const lprefix = `[KDeluxe 2023][${Date.now()}] `; console.log(lprefix + m); };
