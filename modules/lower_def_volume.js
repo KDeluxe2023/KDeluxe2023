60c447e29c3b350b3aa908300a00f0b5e34db11e
KDeluxe2023 <119752397+KDeluxe2023@users.noreply.github.com>
2022-12-09 10:51:17 +0100

    Update lower_def_volume.js

--- modules/lower_def_volume.js
+++ modules/lower_def_volume.js
@@ -1,7 +1,9 @@
-console.log(`[KDeluxe] Lower Default Volume Loaded...`);
-let performance_lower_def_volume = performance.now()
+{
+    console.log(`[KDeluxe] Lower Default Volume Loaded...`);
+    let performance_lower_def_volume = performance.now()
 
-let volume = 0.1
-$("#player").prop("volume", volume);
+    let volume = 0.1
+    $("#player").prop("volume", volume);
 
-console.log(`[KDeluxe] [⏱️] Lower Default Volume loaded in ${performance.now() - performance_lower_def_volume}ms`);
+    console.log(`[KDeluxe] [⏱️] Lower Default Volume loaded in ${performance.now() - performance_lower_def_volume}ms`);
+}
