bb41f71af8d60ec1d9f3bc34d5517917f013bb4b
KDeluxe2023 <119752397+KDeluxe2023@users.noreply.github.com>
2022-12-09 04:26:51 +0100

    Update and rename autoscroll.js to auto_scroll.js

---
+++ modules/auto_scroll.js
@@ -0,0 +1,14 @@
+console.log(`[KDeluxe] Autoscroll Loaded...`);
+let performance_autoscroll = performance.now()
+
+var autoscroll;
+$(".uinfo").before('[<input type="checkbox" class="as-box"/>Autoscroll] '), $(".as-box").change(function() {
+    this.checked ? autoscroll = setInterval(function() {
+        var o = $(".postnew:last");
+        o.length && $("html, body").animate({
+            scrollTop: o.offset().top
+        }, "slow")
+    }, 1e3) : clearInterval(autoscroll)
+})
+
+console.log(`[KDeluxe] [⏱️] Autoscroll loaded in ${performance.now() - performance_autoscroll}ms`);
