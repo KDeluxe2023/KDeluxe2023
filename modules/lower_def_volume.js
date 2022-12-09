844038c0f8a770da725aefcea87e678c520af96d
KDeluxe2023 <119752397+KDeluxe2023@users.noreply.github.com>
2022-12-09 04:20:44 +0100

    Create lower_def_volume.js

---
+++ modules/lower_def_volume.js
@@ -0,0 +1,7 @@
+log("Lower Default Volume Loaded...");
+let performance_lower_def_volume = performance.now()
+
+let volume = 0.1
+$("#player").prop("volume", volume);
+
+console.log(`[KDeluxe] [⏱️] Lower Default Volume loaded in ${performance.now() - performance_lower_def_volume}ms`);
