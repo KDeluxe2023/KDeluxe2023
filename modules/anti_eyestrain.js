b323c3cf88c43ecf3cc7dc5a7537be6726aee276
KDeluxe2023 <119752397+KDeluxe2023@users.noreply.github.com>
2022-12-09 04:11:39 +0100

    Update anti_eyestrain.js

--- modules/anti_eyestrain.js
+++ modules/anti_eyestrain.js
@@ -1,33 +1,31 @@
-if (localStorage.o_kdeluxe_image_preview_anti_eyestrain == 1 && localStorage.o_imgpreview === "1") {
-    console.log(`[KDeluxe] Image Preview Anti-Eyestrain Loaded...`);
-    let performance_anti_eyestrain = performance.now()
+console.log(`[KDeluxe] Image Preview Anti-Eyestrain Loaded...`);
+let performance_anti_eyestrain = performance.now()
 
-    let toggle = false;
-    let preview_container = $("#imagePreview");
-    preview_container.append(`<a href="#" id="anti-eyestrain"><i class="fa fa-eye-slash" aria-hidden="true"></i></a>`);
+let toggle = false;
+let preview_container = $("#imagePreview");
+preview_container.append(`<a href="#" id="anti-eyestrain"><i class="fa fa-eye-slash" aria-hidden="true"></i></a>`);
 
-    $("#anti-eyestrain").css({
-        "position": "absolute",
-        "top": "0",
-        "right": "0",
-        "font-size": "20px",
-        "background-color": "black"
-    });
-    $("#anti-eyestrain").click(function(e) {
-        e.preventDefault();
+$("#anti-eyestrain").css({
+    "position": "absolute",
+    "top": "0",
+    "right": "0",
+    "font-size": "20px",
+    "background-color": "black"
+});
+$("#anti-eyestrain").click(function(e) {
+    e.preventDefault();
 
-        if (toggle) {
-            preview_container.find(">:first-child").css({
-                "filter": ""
-            });
-        } else if (!toggle) {
-            preview_container.find(">:first-child").css({
-                "filter": "hue-rotate(180deg) invert(1)"
-            });
-        }
+    if (toggle) {
+        preview_container.find(">:first-child").css({
+            "filter": ""
+        });
+    } else if (!toggle) {
+        preview_container.find(">:first-child").css({
+            "filter": "hue-rotate(180deg) invert(1)"
+        });
+    }
 
-        toggle = !toggle;
-    });
+    toggle = !toggle;
+});
 
-    console.log(`[KDeluxe] [⏱️] Image Preview Anti-Eyestrain loaded in ${performance.now() - performance_anti_eyestrain}ms`);
-}
+console.log(`[KDeluxe] [⏱️] Image Preview Anti-Eyestrain loaded in ${performance.now() - performance_anti_eyestrain}ms`);
