5a7fca3f8d3859eeead293727067432549db7a10
KDeluxe2023 <119752397+KDeluxe2023@users.noreply.github.com>
2022-12-09 05:20:17 +0100

    Update external_links.js

--- modules/external_links.js
+++ modules/external_links.js
@@ -1,6 +1,29 @@
 console.log(`[KDeluxe] External Links Loaded...`);
 let performance_external_links = performance.now()
 
+inlineVideoAndAudioLinks = function(links) {
+    for (var i = 0; i < links.length; i++) {
+        var url = links[i].getAttribute('href');
+        if (url.indexOf('http://vocaroo.com') > -1) {
+            var vocarooId = url.substr(url.length - 12, 12);
+
+            var vocarooContainer = document.createElement('div');
+            vocarooContainer.innerHTML = '<object width="148" height="44"><param name="movie"' +
+                'value="http://vocaroo.com/player.swf?playMediaID=' + vocarooId + '&autoplay=0"/>' +
+                '<param name="wmode" value="transparent"/>' + '<embed src="http://vocaroo.com/player.swf?playMediaID=' +
+                vocarooId + '&autoplay=0" width="148" ' +
+                'height="44" wmode="transparent" type="application/x-shockwave-flash"></embed></object>';
+
+            if (links[i].nextSibling) {
+                links[i].parentNode.insertBefore(vocarooContainer, links[i].nextSibling);
+            } else {
+                links[i].parentNode.appendChild(vocarooContainer);
+            }
+            links[i].style.display = 'none';
+        }
+    }
+};
+
 var links = document.getElementsByClassName('postlink');
 for (var i = 0; i < links.length; i++) {
     links[i].setAttribute('href', links[i].getAttribute('href').replace('https://href.li/?', ''));
