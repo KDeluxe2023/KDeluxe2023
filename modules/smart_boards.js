ccbac3888e482e85e94f06bea9fcd8f6cdd5c85b
KDeluxe2023 <119752397+KDeluxe2023@users.noreply.github.com>
2022-12-09 10:47:11 +0100

    Update smart_boards.js

--- modules/smart_boards.js
+++ modules/smart_boards.js
@@ -1,21 +1,23 @@
-console.log(`[KDeluxe] Smart Boards Loaded...`);
-let performance_smart_boards = performance.now()
+{
+    console.log(`[KDeluxe] Smart Boards Loaded...`);
+    let performance_smart_boards = performance.now()
 
-let board_ele = $("#tab-boardlink");
+    let board_ele = $("#tab-boardlink");
 
-// yszty kurwa gnoju
-board_ele.find(`[data-short='4']`).remove();
+    // yszty kurwa gnoju
+    board_ele.find(`[data-short='4']`).remove();
 
-// determine if its night
-var date = new Date;
-var hour = date.getHours();
+    // determine if its night
+    let date = new Date;
+    let hour = date.getHours();
 
-if (hour >= 1 && hour <= 5) {
-    // TO-DO: add lightbulb and dim the screen
+    if (hour >= 1 && hour <= 5) {
+        // TO-DO: add lightbulb and dim the screen
 
-} else {
-    // hide /noc/ cause its not available
-    board_ele.find(`[data-short='noc']`).remove();
+    } else {
+        // hide /noc/ cause its not available
+        board_ele.find(`[data-short='noc']`).remove();
+    }
+
+    console.log(`[KDeluxe] [⏱️] Smart Boards loaded in ${performance.now() - performance_smart_boards}ms`);
 }
-
-console.log(`[KDeluxe] [⏱️] Smart Boards loaded in ${performance.now() - performance_smart_boards}ms`);
