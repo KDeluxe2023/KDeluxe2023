1e1bd46ec9737503e4be141ab6ec0fb48660847a
KDeluxe2023 <119752397+KDeluxe2023@users.noreply.github.com>
2022-12-09 23:41:45 +0100

    Create spoiler_revealer.js

---
+++ modules/spoiler_revealer.js
@@ -0,0 +1,13 @@
+{
+  let performance_timer = performance.now();
+  console.log(`[KDeluxe] Spoiler Revealer Loaded...`);
+  
+  $("<style type='text/css'>s { color: white!important; }</style>").appendTo("head");
+          
+  // TO-DO: implement this pseudo-code:
+  // foreach <img src="../img/spoiler.png" alt="Spoiler image" style="width: 170px">
+  // set src to parent <a> href attribute
+  // (do this via mutationobserver)
+  
+  console.log(`[KDeluxe] [⏱️] Spoiler Revealer finished in ${performance.now() - performance_timer}ms`);
+}
