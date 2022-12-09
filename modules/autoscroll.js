447cb9ae85e319d28a7b8c46cdc7b663fead7c58
KDeluxe2023 <119752397+KDeluxe2023@users.noreply.github.com>
2022-12-09 03:17:49 +0100

    Update autoscroll.js

--- modules/autoscroll.js
+++ modules/autoscroll.js
@@ -1,1 +1,16 @@
-papiesz
+if (localStorage.o_kdeluxe_autoscroll == 1 && !g_special_page) {
+    log(`Autoscroll Loaded...`);
+    let performance_autoscroll = performance.now()
+
+    var autoscroll;
+    $(".uinfo").before('[<input type="checkbox" class="as-box"/>Autoscroll] '), $(".as-box").change(function() {
+        this.checked ? autoscroll = setInterval(function() {
+            var o = $(".postnew:last");
+            o.length && $("html, body").animate({
+                scrollTop: o.offset().top
+            }, "slow")
+        }, 1e3) : clearInterval(autoscroll)
+    })
+
+    log(`[⏱️] Autoscroll loaded in ${performance.now() - performance_autoscroll}ms`);
+}
