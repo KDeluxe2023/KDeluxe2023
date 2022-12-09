34287bc60e55a261c1707e9de6b022114723d0af
KDeluxe2023 <119752397+KDeluxe2023@users.noreply.github.com>
2022-12-09 04:10:34 +0100

    Create ban_checker.js

---
+++ modules/ban_checker.js
@@ -0,0 +1,17 @@
+console.log(`[KDeluxe] Ban Checker Loaded...`);
+let performance_ban_checker = performance.now()
+
+$("#postform").after(`<h1 id="banned"></h1>`);
+var ban_check_func = window.setInterval(function() {
+    fetch("https://karachan.org/banned.php").then((resp) => resp.text()).then((text) => {
+        if (!text.includes("NOT BANNED")) {
+            $("#banned").text(`Jesteś zbanowany`);
+            $("#banned").css({
+                "text-align": "center",
+                "color": "red"
+            });
+        }
+    });
+}, 1000 * 10);
+
+console.log(`[KDeluxe] [⏱️] Ban Checker loaded in ${performance.now() - performance_ban_checker}ms`);
