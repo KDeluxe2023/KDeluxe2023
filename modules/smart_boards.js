618fd315bc617a29296efb1ae854dcddd29f19f9
KDeluxe2023 <119752397+KDeluxe2023@users.noreply.github.com>
2022-12-09 04:25:52 +0100

    Create smart_boards.js

---
+++ modules/smart_boards.js
@@ -0,0 +1,21 @@
+console.log(`[KDeluxe] Smart Boards Loaded...`);
+let performance_smart_boards = performance.now()
+
+let board_ele = $("#tab-boardlink");
+
+// yszty kurwa gnoju
+board_ele.find(`[data-short='4']`).remove();
+
+// determine if its night
+var date = new Date;
+var hour = date.getHours();
+
+if (hour >= 1 && hour <= 5) {
+    // TO-DO: add lightbulb and dim the screen
+
+} else {
+    // hide /noc/ cause its not available
+    board_ele.find(`[data-short='noc']`).remove();
+}
+
+console.log(`[KDeluxe] [⏱️] Smart Boards loaded in ${performance.now() - performance_smart_boards}ms`);
