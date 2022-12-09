8d4adc25373799da6828567391a908216830d71f
KDeluxe2023 <119752397+KDeluxe2023@users.noreply.github.com>
2022-12-09 05:14:25 +0100

    Update new_keyframe_anims.js

--- modules/new_keyframe_anims.js
+++ modules/new_keyframe_anims.js
@@ -1,4 +1,4 @@
-log("New Keyframe Animations Loaded...");
+console.log(`[KDeluxe] New Keyframe Animations Loaded...`);
 let performance_new_keyframes = performance.now()
 
 $(`<style type='text/css'>@keyframes robert{from{background-color:green;color:#fff}to{background-color:#fff;color:green}}.maxiu{background-color:red;color:#ff0}</style>`).appendTo("head");
