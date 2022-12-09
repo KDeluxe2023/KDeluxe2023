6176cf515c1b3872ba6ef1625f4705c593904ad7
kotjea2137 <96026027+kotjea2137@users.noreply.github.com>
2022-12-09 14:54:09 +0100

    add draggable radioradio player

--- modules/radio_radio.js
+++ modules/radio_radio.js
@@ -2,34 +2,100 @@ console.log(`[KDeluxe] Radioradio Player Loaded...`);
 let performance_radioradio_player = performance.now()
 
 const d = new Date();
-let day = d.getDay();
-let hour = d.getHours();
+const day = d.getDay();
+const hour = d.getHours();
+const minutes = d.getMinutes();
 
-function display_player(label) {
-    $(`#postform`).after(`
-        <div id="radioradio_player">
-        <figure>
-           <figcaption>SŁUCHAJ AUDYCJI ${label}, TERAZ NA ŻYWO:</figcaption>
-           <audio controls autoplay src="https://c16.radioboss.fm:18014/stream"></audio>
-        </figure>
-        </div>`);
-    $("#radioradio_player").css({
-        "text-align": "center"
-    });
+const displayPlayer = (label) => {
+  const container = document.createElement("div");
+  container.id = "radioradio_player";
+  container.innerHTML = `
+<figure class="radioradio_player_figure">
+   <figcaption style="cursor:pointer;user-select:none;padding-bottom:10px;font-weight:bold;">SŁUCHAJ AUDYCJI ${label}, TERAZ NA ŻYWO:</figcaption>
+   <audio controls src="https://c16.radioboss.fm:18014/stream" preload="metadata"></audio>
+</figure>
+`.trim();
+  container.style.textAlign = "center";
+  container.style.position = "absolute";
+  container.style.border = "solid 1px";
+  container.style.background = "rgba(40,40,40,.5059)";
+  container.style.cursor = "pointer";
+  container.style.userSelect = "none";
+
+  const settings = JSON.parse(localStorage.getItem("radioRadioPlayerPosition"));
+
+  if (settings != null) {
+    container.style.top = settings.offsetTop - settings.pos2 + "px";
+    container.style.left = settings.offsetLeft - settings.pos1 + "px";
+  } else {
+    container.style.top = 10 + "%";
+    container.style.left = 5 + "%";
+  }
+
+  document.querySelector(".boardBanner").after(container);
+};
+
+let lbl = "";
+
+if ((day == 4 && hour >= 22) || (day == 5 && hour <= 5)) lbl = "Info Tydzień";
+
+if ((day == 5 && hour >= 23 && minutes >= 30) || (day == 6 && hour <= 6))
+  lbl = "👽Teoria Chaosu👽";
+
+if (day == 2 && hour >= 21) lbl = "Radioaktywnej";
+
+if (lbl != "") {
+  displayPlayer(lbl);
+  dragElement(document.getElementById("radioradio_player"));
+
+  function dragElement(el) {
+    let pos1 = 0,
+      pos2 = 0,
+      pos3 = 0,
+      pos4 = 0;
+    el.onmousedown = dragMouseDown;
+
+    function dragMouseDown(e) {
+      e = e || window.event;
+      e.preventDefault();
+
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+
+      document.onmouseup = closeDragElement;
+      document.onmousemove = elementDrag;
+    }
+
+    function elementDrag(e) {
+      e = e || window.event;
+      e.preventDefault();
+
+      pos1 = pos3 - e.clientX;
+      pos2 = pos4 - e.clientY;
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+
+      el.style.top = el.offsetTop - pos2 + "px";
+      el.style.left = el.offsetLeft - pos1 + "px";
+    }
+
+    function closeDragElement() {
+      document.onmouseup = null;
+      document.onmousemove = null;
+
+      localStorage.setItem(
+        "radioRadioPlayerPosition",
+        JSON.stringify({
+          pos1: pos1,
+          pos2: pos2,
+          pos3: pos3,
+          pos4: pos4,
+          offsetTop: el.offsetTop,
+          offsetLeft: el.offsetLeft,
+        })
+      );
+    }
+  }
 }
-
-let lbl = null;
-
-if (day == 4 && hour >= 22 || day == 5 && hour <= 5)
-    lbl = "Info Tydzień";
-
-if (day == 6 && hour >= 0 && hour <= 6)
-    lbl = "👽Teoria Chaosu👽";
-
-if (day == 2 && hour >= 21)
-    lbl = "Radioaktywnej";
-
-if (lbl != null)
-    display_player(lbl)
 
 console.log(`[KDeluxe] [⏱️] RadioRadio Player loaded in ${performance.now() - performance_radioradio_player}ms`);
