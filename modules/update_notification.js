122cd114e302d96e5f7fb378cd377dbf56c1b78e
KDeluxe2023 <119752397+KDeluxe2023@users.noreply.github.com>
2022-12-11 10:32:23 +0100

    Update update_notification.js

--- modules/update_notification.js
+++ modules/update_notification.js
@@ -1,3 +1,20 @@
 {
-  
+    $.ajax({
+        url: `https://raw.githubusercontent.com/KDeluxe2023/KDeluxe2023/main/version.txt`,
+        async: false,
+        cache: false,
+        success: function(data) {
+            let local_version = $.trim(GM.info.script.version);
+            let remote_version = $.trim(data);
+
+            if (local_version != remote_version) {
+                dialogBox('KDeluxe', `Dostępna jest nowsza wersja KDeluxe (${remote_version})<br/>Czy chcesz ją zainstalować?`, ["Tak", "Nie"], 'fa-code-fork', function(a) {
+                    if (a == 1)
+                        return;
+
+                    window.location.href = `https://github.com/KDeluxe2023/KDeluxe2023/raw/main/karachan_deluxe2023.user.js`;
+                });
+            }
+        }
+    });
 }
