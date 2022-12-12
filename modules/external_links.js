a7a618db88217169c59b70ab4301267806915d37
KDeluxe2023 <119752397+KDeluxe2023@users.noreply.github.com>
2022-12-12 13:57:21 +0100

    Update external_links.js

--- modules/external_links.js
+++ modules/external_links.js
@@ -1,36 +1,13 @@
-console.log(`[KDeluxe] External Links Loaded...`);
-let performance_external_links = performance.now()
+{
+    console.log(`[KDeluxe] External Links Loaded...`);
+    let performance_external_links = performance.now()
 
-inlineVideoAndAudioLinks = function(links) {
+    var links = document.getElementsByClassName('postlink');
     for (var i = 0; i < links.length; i++) {
-        var url = links[i].getAttribute('href');
-        if (url.indexOf('http://vocaroo.com') > -1) {
-            var vocarooId = url.substr(url.length - 12, 12);
-
-            var vocarooContainer = document.createElement('div');
-            vocarooContainer.innerHTML = '<object width="148" height="44"><param name="movie"' +
-                'value="http://vocaroo.com/player.swf?playMediaID=' + vocarooId + '&autoplay=0"/>' +
-                '<param name="wmode" value="transparent"/>' + '<embed src="http://vocaroo.com/player.swf?playMediaID=' +
-                vocarooId + '&autoplay=0" width="148" ' +
-                'height="44" wmode="transparent" type="application/x-shockwave-flash"></embed></object>';
-
-            if (links[i].nextSibling) {
-                links[i].parentNode.insertBefore(vocarooContainer, links[i].nextSibling);
-            } else {
-                links[i].parentNode.appendChild(vocarooContainer);
-            }
-            links[i].style.display = 'none';
-        }
+        links[i].setAttribute('href', links[i].getAttribute('href').replace('https://href.li/?', ''));
+        links[i].setAttribute('target', '_blank');
+        links[i].setAttribute('rel', 'noreferrer');
     }
-};
 
-var links = document.getElementsByClassName('postlink');
-for (var i = 0; i < links.length; i++) {
-    links[i].setAttribute('href', links[i].getAttribute('href').replace('https://href.li/?', ''));
-    links[i].setAttribute('target', '_blank');
-    links[i].setAttribute('rel', 'noreferrer');
+    console.log(`[KDeluxe] [⏱️] External Links loaded in ${performance.now() - performance_external_links}ms`);
 }
-
-this.inlineVideoAndAudioLinks(links);
-
-console.log(`[KDeluxe] [⏱️] External Links loaded in ${performance.now() - performance_external_links}ms`);
