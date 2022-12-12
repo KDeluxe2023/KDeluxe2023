29204898b53d519b365422a3abfdf32a8d67ff75
KDeluxe2023 <119752397+KDeluxe2023@users.noreply.github.com>
2022-12-12 13:55:19 +0100

    Update auto_follow.js

--- modules/auto_follow.js
+++ modules/auto_follow.js
@@ -1,13 +1,15 @@
-console.log(`[KDeluxe] Auto Follow Loaded...`);
-let performance_autofollow = performance.now()
+{
+    console.log(`[KDeluxe] Auto Follow Loaded...`);
+    let performance_autofollow = performance.now()
 
-$('#postform').on('submit', function(e) {
-    e.preventDefault();
-    let watchlink = $(".watch-button-container a")[0];
-    if (watchlink.innerText.trim() == "[Obserwuj]") {
-        watchlink.click();
-        console.log(`[KDeluxe] Auto Follow Executed`);
-    }
-});
+    $('#postform').on('submit', function(e) {
+        e.preventDefault();
+        let watchlink = $(".watch-button-container a")[0];
+        if (watchlink.innerText.trim() == "[Obserwuj]") {
+            watchlink.click();
+            console.log(`[KDeluxe] Auto Follow Executed`);
+        }
+    });
 
-console.log(`[KDeluxe] [⏱️] Autofollow loaded in ${performance.now() - performance_autofollow}ms`);
+    console.log(`[KDeluxe] [⏱️] Autofollow loaded in ${performance.now() - performance_autofollow}ms`);
+}
