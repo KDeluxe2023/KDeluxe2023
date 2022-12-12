3907c031bd5c13d48378df0a54e10a56cc5630a5
KDeluxe2023 <119752397+KDeluxe2023@users.noreply.github.com>
2022-12-12 14:02:34 +0100

    Update password_changer.js

--- modules/password_changer.js
+++ modules/password_changer.js
@@ -1,13 +1,15 @@
-console.log(`[KDeluxe] Password Changer Loaded...`);
-let performance_password_changer = performance.now()
+{
+    console.log(`[KDeluxe] Password Changer Loaded...`);
+    let performance_password_changer = performance.now()
 
-function random_str(r) {
-    for (var n = "", o = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789", t = o.length, a = 0; a < r; a++) n += o.charAt(Math.floor(Math.random() * t));
-    return n
+    function random_str(r) {
+        for (var n = "", o = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789", t = o.length, a = 0; a < r; a++) n += o.charAt(Math.floor(Math.random() * t));
+        return n
+    }
+
+    $.cookie("password", random_str(8), {
+        path: '/'
+    });
+    console.log(`[KDeluxe] Password changed: ${$.cookie("password")}`);
+    console.log(`[KDeluxe] [⏱️] Password Changer loaded in ${performance.now() - performance_password_changer}ms`);
 }
-
-$.cookie("password", random_str(8), {
-    path: '/'
-});
-console.log(`[KDeluxe] Password changed: ${$.cookie("password")}`);
-console.log(`[KDeluxe] [⏱️] Password Changer loaded in ${performance.now() - performance_password_changer}ms`);
