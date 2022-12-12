a73b0a1b2c89c31abd518697ce7b8b930eb15136
KDeluxe2023 <119752397+KDeluxe2023@users.noreply.github.com>
2022-12-12 13:54:05 +0100

    Update catalog_curb.js

--- modules/catalog_curb.js
+++ modules/catalog_curb.js
@@ -1,33 +1,35 @@
-console.log(`[KDeluxe] Catalog Curb Loaded...`);
-let performance_catalog_curb = performance.now()
+{
+    console.log(`[KDeluxe] Catalog Curb Loaded...`);
+    let performance_catalog_curb = performance.now()
 
-let board = window.location.toString().split("/")[3];
-let posts = document.querySelectorAll(".thread");
+    let board = window.location.toString().split("/")[3];
+    let posts = document.querySelectorAll(".thread");
 
-// TO-DO: dont store curbed threads individually, use json with curb time and id+board instead
-function getHideValue(el) {
-    let threadId = el.getAttribute("id").slice(7);
-    return `h_${board}_${threadId}`;
-}
-
-posts.forEach((el, idx) => {
-    for (let key in localStorage) {
-        if (key == getHideValue(el)) {
-            el.remove();
-            break;
-        }
+    // TO-DO: dont store curbed threads individually, use json with curb time and id+board instead
+    function getHideValue(el) {
+        let threadId = el.getAttribute("id").slice(7);
+        return `h_${board}_${threadId}`;
     }
 
-    el.innerHTML = `<div><a class="hide" href="#" style="font-size:21px;">[–]</a></div>` + el.innerHTML
-});
+    posts.forEach((el, idx) => {
+        for (let key in localStorage) {
+            if (key == getHideValue(el)) {
+                el.remove();
+                break;
+            }
+        }
 
-document.querySelectorAll(".hide").forEach((el) => {
-    el.addEventListener("click", (e) => {
-        e.preventDefault();
-        let hideVal = getHideValue(el.parentElement.parentElement);
-        localStorage.setItem(hideVal, "x");
-        el.parentElement.parentElement.remove();
+        el.innerHTML = `<div><a class="hide" href="#" style="font-size:21px;">[–]</a></div>` + el.innerHTML
     });
-});
 
-console.log(`[KDeluxe] [⏱️] Catalog Curb loaded in ${performance.now() - performance_catalog_curb}ms`);
+    document.querySelectorAll(".hide").forEach((el) => {
+        el.addEventListener("click", (e) => {
+            e.preventDefault();
+            let hideVal = getHideValue(el.parentElement.parentElement);
+            localStorage.setItem(hideVal, "x");
+            el.parentElement.parentElement.remove();
+        });
+    });
+
+    console.log(`[KDeluxe] [⏱️] Catalog Curb loaded in ${performance.now() - performance_catalog_curb}ms`);
+}
