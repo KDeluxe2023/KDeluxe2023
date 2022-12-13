cc2d6b1c7817f7b066718bdbf4458470123c499d
KDeluxe2023 <119752397+KDeluxe2023@users.noreply.github.com>
2022-12-13 17:30:21 +0100

    Update auto_scroll.js

--- modules/auto_scroll.js
+++ modules/auto_scroll.js
@@ -1,7 +1,31 @@
 {
     console.log(`[KDeluxe] Autoscroll Loaded...`);
-    let performance_autoscroll = performance.now()
+    let performance_timer = performance.now()
 
+    /*
+    PUREJS:
+    const uInfo = document.querySelectorAll('.uinfo');
+uInfo.forEach(element => {
+    element.insertAdjacentHTML('beforebegin', `[<input type="checkbox" class="as-box"/>Autoscroll]`);
+});
+
+
+input.addEventListener('change', function() {
+    if (this.checked) {
+        autoscroll = setInterval(function() {
+            const postNew = document.querySelector('.postnew:last-child');
+            if (postNew) {
+                window.scroll({
+                    top: postNew.offset().top,
+                    behavior: 'smooth'
+                });
+            }
+        }, 1000);
+    } else {
+        clearInterval(autoscroll);
+    }
+});
+    */
     var autoscroll;
     $(".uinfo").before('[<input type="checkbox" class="as-box"/>Autoscroll] '), $(".as-box").change(function() {
         this.checked ? autoscroll = setInterval(function() {
@@ -12,5 +36,5 @@
         }, 1e3) : clearInterval(autoscroll)
     })
 
-    console.log(`[KDeluxe] [⏱️] Autoscroll loaded in ${performance.now() - performance_autoscroll}ms`);
+    console.log(`[KDeluxe] [⏱️] Autoscroll loaded in ${performance.now() - performance_timer}ms`);
 }
