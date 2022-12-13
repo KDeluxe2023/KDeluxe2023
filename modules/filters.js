2d9be76d7100c4f7fb18214dd1829eb2f6f87622
KDeluxe2023 <119752397+KDeluxe2023@users.noreply.github.com>
2022-12-13 10:41:33 +0100

    Update filters.js

--- modules/filters.js
+++ modules/filters.js
@@ -2,60 +2,55 @@
     console.log(`[KDeluxe] Heuristic Filters Initiated...`);
     let performance_timer = performance.now()
 
-    let rcount = 0;
-
     // accept TOS
     setCookie('regulamin', 'accepted', 365);
 
     // remove unwanted elements
-    const ele_blacklist = ["#smok", "#jesli-zablokujesz-tego-diva-ukraina-odniesie-zwyciestwo", "#regulamin", ".absBotDisclaimer"]
-    ele_blacklist.forEach(function(item, index) {
-        if ($(item).length) {
-            $(item).remove();
-            rcount++;
-        }
-    });
-
-    // hide captcha badget
-    if ($(".grecaptcha-badge").length) {
-        $(".grecaptcha-badge").hide();
-        rcount++;
-    }
+    document.getElementById("smok").style.display = 'none';
+    document.getElementById("jesli-zablokujesz-tego-diva-ukraina-odniesie-zwyciestwo").style.display = 'none';
+    document.getElementById("regulamin").style.display = 'none';
+    document.getElementsByClassName('absBotDisclaimer')[0].style.display = 'none';
+    document.getElementsByClassName('grecaptcha-badge')[0].style.display = 'none';
 
     // remove invisible iframes
-    $("iframe").each(function() {
-        let src = $(this).attr('src');
+    const iframes = document.querySelectorAll('iframe');
+    iframes.forEach(function(item) {
+        let src = item.getAttribute('src');
+
         if (src == undefined)
             return true;
 
         // skip grecaptcha
-        if (src.indexOf("www.google.com/recaptcha/") >= 0)
+        if (src.indexOf("www.google.com/recaptcha/") >= 0) {
             return true;
+        }
 
-        let width = $(this).attr('width');
-        let height = $(this).attr('width');
+        let width = item.getAttribute('width');
+        let height = item.getAttribute('width');
 
         if (width < 5 || height < 5) {
-            $(this).remove();
+            item.parentNode.removeChild(item);
             rcount++;
-
-            log("Dropped invisible iframe: " + src);
         }
     });
 
     // Anti: Malicious CSS
-    $(`<style type='text/css'>.anti_css { transform: rotate(0deg) !important; }</style>`).appendTo("head");
+    const style = document.createElement('style');
+    style.innerHTML = ".anti_css { transform: rotate(0deg) !important; }";
+    document.head.appendChild(style);
 
-    const revert_ele = [".board"]
-    revert_ele.forEach(function(item, index) {
-        if ($(item).length) {
-            $(item).addClass("anti_css");
+    const revert_ele = [".board"];
+    revert_ele.forEach(function(item) {
+        const elements = document.querySelectorAll(item);
+        if (elements.length > 0) {
+            Array.prototype.forEach.call(elements, function(element) {
+                element.classList.add('anti_css');
+            });
         }
     });
 
     // Anti-wirówka
     localStorage.xD = 'xD';
 
-    console.log(`[KDeluxe] Filtered ${rcount} elements!`);
     console.log(`[KDeluxe] [⏱️] Heuristic Filters loaded in ${performance.now() - performance_timer}ms`);
 }
