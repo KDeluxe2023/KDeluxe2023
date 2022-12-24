5abb829ee3f84d28e3341f27c03a8cd44a572415
KDeluxe2023 <119752397+KDeluxe2023@users.noreply.github.com>
2022-12-24 02:42:26 +0100

    Update ban_checker.js

--- modules/ban_checker.js
+++ modules/ban_checker.js
@@ -1,19 +1,31 @@
 {
     console.log(`[KDeluxe] Ban Checker Loaded...`);
-    let performance_ban_checker = performance.now()
+    let performance_timer = performance.now()
 
-    $("#postform").after(`<h1 id="banned"></h1>`);
-    var ban_check_func = window.setInterval(function() {
-        fetch("https://karachan.org/banned.php").then((resp) => resp.text()).then((text) => {
-            if (!text.includes("NOT BANNED")) {
-                $("#banned").text(`Jesteś zbanowany`);
-                $("#banned").css({
-                    "text-align": "center",
-                    "color": "red"
-                });
+    // create red info label tellin you that you're banned
+    const postform = document.querySelector('#postform');
+    const banned = document.createElement('h1');
+    banned.id = 'banned';
+    postform.insertAdjacentHTML('afterend', banned.outerHTML);
+
+    // send http request to banned.php
+    function check_for_ban() {
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", "https://karachan.org/banned.php");
+        xhr.onload = function() {
+            if (!this.responseText.includes("NOT BANNED")) {
+                var bannedElement = document.getElementById("banned");
+                bannedElement.textContent = "Jesteś zbanowany";
+                bannedElement.style.textAlign = "center";
+                bannedElement.style.color = "red";
             }
-        });
-    }, 1000 * 10);
+        };
+        xhr.send();
 
-    console.log(`[KDeluxe] [⏱️] Ban Checker loaded in ${performance.now() - performance_ban_checker}ms`);
+        setTimeout(check_for_ban, 1000 * 10);
+    }
+
+    check_for_ban();
+
+    console.log(`[KDeluxe] [⏱️] Ban Checker loaded in ${performance.now() - performance_timer}ms`);
 }
