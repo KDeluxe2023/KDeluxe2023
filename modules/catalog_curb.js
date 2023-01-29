94534b0e1053ed479c0612ff8898393eaabf50ad
BiscuitKrueger <108426474+BiscuitKrueger@users.noreply.github.com>
2023-01-29 18:42:39 +0100

    Use JSON to store the curbed threads

--- modules/catalog_curb.js
+++ modules/catalog_curb.js
@@ -1,35 +1,36 @@
 {
     console.log(`[KDeluxe] Catalog Curb Loaded...`);
-    let performance_catalog_curb = performance.now()
+let performance_catalog_curb = performance.now();
 
-    let board = window.location.toString().split("/")[3];
-    let posts = document.querySelectorAll(".thread");
+let board = window.location.toString().split("/")[3];
+let posts = document.querySelectorAll(".thread");
+let curbedThreads = JSON.parse(localStorage.getItem("curbedThreads")) || {};
 
-    // TO-DO: dont store curbed threads individually, use json with curb time and id+board instead
-    function getHideValue(el) {
-        let threadId = el.getAttribute("id").slice(7);
-        return `h_${board}_${threadId}`;
+function getThreadId(el) {
+  return el.getAttribute("id").slice(7);
+}
+
+posts.forEach((el, idx) => {
+  let threadId = getThreadId(el);
+  if (curbedThreads[board] && curbedThreads[board].includes(threadId)) {
+    el.remove();
+  } else {
+    el.innerHTML = `<div><a class="hide" href="#" style="font-size:21px;">[–]</a></div>` + el.innerHTML;
+  }
+});
+
+document.querySelectorAll(".hide").forEach((el) => {
+  el.addEventListener("click", (e) => {
+    e.preventDefault();
+    let threadId = getThreadId(el.parentElement.parentElement);
+    if (!curbedThreads[board]) {
+      curbedThreads[board] = [];
     }
+    curbedThreads[board].push(threadId);
+    localStorage.setItem("curbedThreads", JSON.stringify(curbedThreads));
+    el.parentElement.parentElement.remove();
+  });
+});
 
-    posts.forEach((el, idx) => {
-        for (let key in localStorage) {
-            if (key == getHideValue(el)) {
-                el.remove();
-                break;
-            }
-        }
-
-        el.innerHTML = `<div><a class="hide" href="#" style="font-size:21px;">[–]</a></div>` + el.innerHTML
-    });
-
-    document.querySelectorAll(".hide").forEach((el) => {
-        el.addEventListener("click", (e) => {
-            e.preventDefault();
-            let hideVal = getHideValue(el.parentElement.parentElement);
-            localStorage.setItem(hideVal, "x");
-            el.parentElement.parentElement.remove();
-        });
-    });
-
-    console.log(`[KDeluxe] [⏱️] Catalog Curb loaded in ${performance.now() - performance_catalog_curb}ms`);
+console.log(`[KDeluxe] [⏱️] Catalog Curb loaded in ${performance.now() - performance_catalog_curb}ms`);
 }
