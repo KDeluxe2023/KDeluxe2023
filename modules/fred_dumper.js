7151700d6c77688843ca5a6c27ce7d0b06cbc111
Biscuit Krueger <108426474+BiscuitKrueger@users.noreply.github.com>
2023-01-29 22:44:14 +0100

    Update fred_dumper.js

--- modules/fred_dumper.js
+++ modules/fred_dumper.js
@@ -1,90 +1,67 @@
 {
-    console.log(`[KDeluxe] Fred Dumper Loaded...`);
-    let performance_fred_dumper = performance.now()
+console.log("[KDeluxe] Fred Dumper Loaded...");
+const performanceFredDumper = performance.now();
+const $post = $('.post').first();
+const $postInfo = $post.find('.postInfo').first();
+$postInfo.prepend('<span id="dumper_container">[<a href="#" id="dump_thread"><i class="fa fa-download" aria-hidden="true"></i></a>]</span>');
+$("#dumper_container").css({ marginRight: "3px" });
 
-    let bar = $('.post').first().find('.postInfo').first();
-    bar.prepend(`<span id="dumper_container">[<a href="#" id="dump_thread"><i class="fa fa-download" aria-hidden="true"></i></a>]</span>`);
-    $("#dumper_container").css({
-        "margin-right": "3px"
-    });
+$("#dump_thread").click(function(e) {
+    e.preventDefault();
+    dialogBox('KDeluxe', 'Jak chcesz pobrać ten temat?', ["Screenshot", "Screenshot + Pliki", "Tylko Pliki", "Anuluj"], 'fa-download', function(a) {
+        if (a === 3) return;
 
-    // TO-DO: load required libraries for this on demand instead doing it on every script load
-    $("#dump_thread").click(function(e) {
-        e.preventDefault();
-        dialogBox('KDeluxe', `Jak chcesz pobrać ten temat?`, ["Screenshot", "Screenshot + Pliki", "Tylko Pliki", "Anuluj"], 'fa-download', function(a) {
-            // cancel
-            if (a == 3)
-                return;
+        const fredId = $postInfo.attr("id").replace(/\D/g, '');
+        const fredTime = $postInfo.find(".dateTime").attr("title");
 
-            let fred_id = bar.attr("id").replace(/\D/g, '');
-            let fred_time = bar.find(".dateTime").attr("title");
+        if (a === 2) {
+            downloadAllMediaZipped();
+            return;
+        }
 
-            // download files only
-            if (a == 2) {
-                download_all_media_zipped();
-                return;
-            }
+        const fredHeight = document.body.scrollHeight;
+        if (fredHeight > 32767) {
+            dialogBox('KDeluxe', 'Ten fred przekracza 32,000px wysokości i nie może zostać zeskrinowany', ['OK'], 'fa-exclamation-triangle');
+            return;
+        }
 
-            // check if created canvas will fit within browser's limit
-            let fred_height = document.body.scrollHeight;
-            if (fred_height > 32767) {
-                dialogBox('KDeluxe', `Ten fred przekracza 32,000px wysokości i nie może zostać zeskrinowany`, ['OK'], 'fa-exclamation-triangle');
-                return;
-            }
-
-            function download_all_media_zipped() {
-                function saveToZip(e, t) {
-                    log("Generating zip...");
-                    const n = new JSZip,
-                        o = n.folder("project");
-                    t.forEach((e => {
-                        const t = fetch(e).then((e => 200 === e.status ? e.blob() : Promise.reject(new Error(e.statusText)))),
-                            n = e.substring(e.lastIndexOf("/"));
-                        o.file(n, t)
-                    })), n.generateAsync({
-                        type: "blob"
-                    }).then((t => saveAs(t, e))).catch((e => console.log(e)))
-                }
-
-                log("Creating zip...");
-                let urls = [];
-                $('.fileThumb').each(function() {
-                    let link = $(this).attr("href");
-                    if (link == undefined)
-                        return true;
-
-                    let absolute_url = new URL(link, document.baseURI).href;
-
-                    urls.push(absolute_url);
+        function downloadAllMediaZipped() {
+            function saveToZip(name, urls) {
+                console.log("Generating zip...");
+                const zip = new JSZip();
+                const folder = zip.folder("project");
+                urls.forEach(url => {
+                    const file = fetch(url)
+                        .then(res => (res.status === 200 ? res.blob() : Promise.reject(new Error(res.statusText))))
+                        .then(blob => {
+                            const fileName = url.substring(url.lastIndexOf("/"));
+                            folder.file(fileName, blob);
+                        });
                 });
-                console.log(`[KDeluxe] ${urls.length} links to zip collected`);
-
-                saveToZip(`${fred_id} - ${fred_time}.zip`, urls)
+                zip.generateAsync({ type: "blob" })
+                    .then(blob => saveAs(blob, name))
+                    .catch(err => console.log(err));
             }
 
-            // screenshot or screenshot+download files
-            if (a == 0 || a == 1) {
-                function downloadURI(e, d) {
-                    var o = document.createElement("a");
-                    o.download = d, o.href = e, document.body.appendChild(o), o.click(), document.body.removeChild(o), delete o
-                };
+            console.log("Creating zip...");
+            const urls = [];
+            $('.fileThumb').each(function() {
+                const link = $(this).attr("href");
+                if (!link) return true;
+                const absoluteUrl = new URL(link, document.baseURI).href;
+                urls.push(absoluteUrl);
+            });
+            console.log(`[KDeluxe] ${urls.length} links to zip collected`);
+            saveToZip(`${fredId} - ${fredTime}.zip`, urls);
+        }
 
-                // wait for dialogbox to disappear
-                setTimeout(function() {
-                    html2canvas(document.body).then(function(canvas) {
-                        var dt = canvas.toDataURL('image/jpeg');
-                        downloadURI(dt, `${fred_id} - ${fred_time}.jpg`);
-                    });
-                }, 1000);
-
-                // download files
-                if (a == 1) {
-                    download_all_media_zipped();
-                }
-            }
-
-        });
-    });
-
-    console.log(`[KDeluxe] [⏱️] Fred Dumper loaded in ${performance.now() - performance_fred_dumper}ms`);
-}
+        if (a === 0 || a === 1) {
+            function downloadURI(uri, name) {
+                const link = document.createElement("a");
+                link.download = name;
+                link.download = name;
+                link.href = uri;
+                document.body.appendChild(link);
+                link.click();
+                document.body.removeChild(link);
+                delete link;
