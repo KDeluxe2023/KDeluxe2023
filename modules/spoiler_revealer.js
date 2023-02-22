84ed9b1c7ea4441d376871c05e00b73879cfc240
KDeluxe2023 <119752397+KDeluxe2023@users.noreply.github.com>
2023-02-22 07:29:03 +0100

    Update spoiler_revealer.js

--- modules/spoiler_revealer.js
+++ modules/spoiler_revealer.js
@@ -9,10 +9,19 @@
         })
     );
 
-    // TO-DO: implement this pseudo-code:
-    // foreach <img src="../img/spoiler.png" alt="Spoiler image" style="width: 170px">
-    // set src to parent <a> href attribute
-    // (do this via mutationobserver)
+    // Get all the <img> elements with a src attribute that ends with "/img/spoiler.png"
+    const spoilerImages = document.querySelectorAll('img[src$="/img/spoiler.png"]');
+
+    // Loop through each <img> element
+    spoilerImages.forEach(function(img) {
+        // Get the parent <a> element
+        const a = img.parentNode;
+        // If the parent element is an <a> element and has a class of "fileThumb"
+        if (a.nodeName === 'A' && a.classList.contains('fileThumb')) {
+            // Set the src attribute of the <img> element to the href of the <a> element
+            img.setAttribute('src', a.getAttribute('href'));
+        }
+    });
 
     console.log(`[KDeluxe] [⏱️] Spoiler Revealer finished in ${performance.now() - performance_timer}ms`);
 }
