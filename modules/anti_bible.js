0a76c109e09baec46a4fa3459b0ffaed96ed298b
KDeluxe2023 <119752397+KDeluxe2023@users.noreply.github.com>
2023-02-22 07:40:46 +0100

    Update anti_bible.js

--- modules/anti_bible.js
+++ modules/anti_bible.js
@@ -2,21 +2,29 @@
     console.log(`[KDeluxe] Anti-bible Loaded...`);
     let performance_timer = performance.now()
 
-    (async function() {
-        try {
-            if (document.URL) {
-                const response = await fetch(document.URL);
-                const text = await response.text();
-                const doc = document.implementation.createHTMLDocument();
-                doc.write(text);
-                const posts = document.getElementsByTagName("blockquote");
-                for (const post of posts)
-                    post.innerHTML = doc.getElementById(post.id).innerHTML;
-            }
-        } catch (err) {
-            console.error(err);
-        }
-    }());
+    // first make sure htmlshiv.js is loaded, so we're not make unnecessary request
+    const isScriptLoaded = filename => new Promise(resolve => (document.head.appendChild(Object.assign(document.createElement('script'), {src: filename, onload: () => resolve(true), onerror: () => resolve(false)}))));
+    isScriptLoaded('https://karachan.org/js/htmlshiv.js').then((result) => {
+        if (result) {
+            (async function() {
+                try {
+                    if (document.URL) {
+                        const response = await fetch(document.URL);
+                        const text = await response.text();
+                        const doc = document.implementation.createHTMLDocument();
+                        doc.write(text);
+                        const posts = document.getElementsByTagName("blockquote");
+                        for (const post of posts)
+                            post.innerHTML = doc.getElementById(post.id).innerHTML;
 
-    console.log(`[KDeluxe] [⏱️] Anti-bible loaded in ${performance.now() - performance_timer}ms`);
+                        console.log(`[KDeluxe] [⏱️] Anti-bible loaded in ${performance.now() - performance_timer}ms`);
+                    }
+                } catch (err) {
+                    console.error(`[KDeluxe] ${err}`);
+                }
+            }());
+        } else {
+            console.error(`[KDeluxe] Anti-bible didnt load because htmlshiv.js was already unloaded`);
+        }
+    });
 }
