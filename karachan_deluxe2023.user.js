89d7aa73b532e11828981d81db927908a94166fb
KDeluxe2023 <119752397+KDeluxe2023@users.noreply.github.com>
2023-04-07 04:51:10 +0200

    Update karachan_deluxe2023.user.js

--- karachan_deluxe2023.user.js
+++ karachan_deluxe2023.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Karachan Deluxe 2023
 // @namespace    karachan.org
-// @version      0.8.8
+// @version      0.8.9
 // @updateURL https://github.com/KDeluxe2023/KDeluxe2023/raw/main/karachan_deluxe2023.user.js
 // @downloadURL https://github.com/KDeluxe2023/KDeluxe2023/raw/main/karachan_deluxe2023.user.js
 
@@ -18,59 +18,68 @@
 // @grant        none
 // ==/UserScript==
 // modules will be loaded at this commit in github repo via jsdelivr
-const g_last_commit = "d7786048f5b455adad747c763754c75eb1d07c3d";
+const g_last_commit = "57105e35965cc4620d2777b8fd95b54dd5eec6cf";
 const g_script_version = GM.info.script.version;
 
 // dynamic module loader
 function load_module(module_name, callback_fn, data_pass = "") {
+    console.time(`Loading ${module_name}`);
     var a = document.createElement("script"),
         n = document.getElementsByTagName("script")[0];
     a.async = 1, a.onload = a.onreadystatechange = function(e, n) {
-        (n || !a.readyState || /loaded|complete/.test(a.readyState)) && (a.onload = a.onreadystatechange = null, a = void 0, !n && callback_fn && setTimeout(callback_fn, 0))
-    }, a.onerror = function(error) {
-        alert(`KDeluxe napotkał błąd przy ładowaniu modułu: ${error.target.src}`);
-    }, a.src = `https://cdn.jsdelivr.net/gh/KDeluxe2023/KDeluxe2023@${g_last_commit}/${module_name}.js`, a.setAttribute("data-pass", `${data_pass}`), n.parentNode.insertBefore(a, n)
+            (n || !a.readyState || /loaded|complete/.test(a.readyState)) && (a.onload = a.onreadystatechange = null, a = void 0, !n && callback_fn && setTimeout(callback_fn, 0), console.timeEnd(`Loading ${module_name}`))
+        },
+        a.onerror = function(error) {
+            alert(`KDeluxe napotkał błąd przy ładowaniu modułu: ${error.target.src}`);
+        },
+        a.src = `https://cdn.jsdelivr.net/gh/KDeluxe2023/KDeluxe2023@${g_last_commit}/${module_name}.js`, a.setAttribute("data-pass", `${data_pass}`),
+        n.parentNode.insertBefore(a, n);
 }
 
-//// globals
-// tells us if we're on a special page like search etc
-var g_special_page = false;
-// tells us if we're in catalog view
-var g_is_in_catalog = false;
-// tells us if we have a topic fully opened
-var g_is_fred_open = false;
-// holds user selected style
-var g_style = localStorage.style;
+//// identify special pages
+{
+    // defines
+    const special_pages = {
+        "catalog.html": "catalog",
+        "search.php": "search",
+        "/rs/": "rs",
+        "/*/": "overboard",
+        "/b/": "/b/"
+    };
 
-// assign globals
-let special_pages = ["catalog.html", "search.php", "/rs/", "/*/"];
-let url_path = window.location.pathname;
-for (const special_page_indicator of special_pages) {
-    if (url_path.endsWith(special_page_indicator)) {
-        g_special_page = true;
+    // current URL path
+    const path = window.location.pathname;
 
-        // detect if we're in catalog
-        if (url_path.endsWith("catalog.html"))
-            g_is_in_catalog = true;
-    }
+    // determine which special page the URL belongs to
+    const special_page = Object.entries(special_pages).find(([page]) =>
+        path.endsWith(page)
+    )?.[1];
+
+    // setup globals
+    window.page_type = special_page ? special_page : "default";
+    console.log(`window.page_type = ${window.page_type}`);
+
+    window.page_special = special_page === "search" ||
+        special_page === "catalog" ||
+        special_page === "rs" ?
+        true : false;
+    console.log(`window.page_special = ${window.page_special}`);
+
+    window.fred_opened = window.location.toString().includes("/res/") ? true : false;
+    console.log(`window.fred_opened = ${window.fred_opened}`);
 }
-
-if (window.location.toString().includes("/res/"))
-    g_is_fred_open = true;
-
 // actual script begins here
 document.addEventListener('readystatechange', event => {
     if (event.target.readyState !== "complete")
         return;
-
     // draw version info
     document.querySelector(".group-options").insertAdjacentHTML('beforeend', `<div style="font-size: 10px;position:absolute">[KDeluxe v${g_script_version}]</div>`);
 
     // run filters first
     if (localStorage.o_kdeluxe_advanced_filters == 1)
         load_module("modules/filters");
-    
-    if (localStorage.o_kdeluxe_anti_bible == 1 && !g_special_page)
+
+    if (localStorage.o_kdeluxe_anti_bible == 1 && !window.page_special)
         load_module("modules/anti_bible");
 
     // check for update (this is only a notification)
@@ -81,53 +90,50 @@ document.addEventListener('readystatechange', event => {
         if (localStorage.o_kdeluxe_smart_boards == 1)
             load_module("modules/smart_boards");
 
-        if (localStorage.o_kdeluxe_enhanced_postform == 1 && !g_special_page)
+        if (localStorage.o_kdeluxe_enhanced_postform == 1 && !window.page_special)
             load_module("modules/enhanced_postform");
 
-        if (localStorage.o_kdeluxe_clickable_boardname == 1 && !g_special_page)
+        if (localStorage.o_kdeluxe_clickable_boardname == 1 && !window.page_special)
             load_module("modules/clickable_boardname");
-        
+
         if (localStorage.o_kdeluxe_quick_search == 1)
             load_module("modules/quick_search");
 
         if (localStorage.o_kdeluxe_community_styles == 1)
             load_module("modules/community_styles");
 
-        if (localStorage.o_kdeluxe_rich_stats == 1 && !g_special_page)
+        if (localStorage.o_kdeluxe_rich_stats == 1 && !window.page_special)
             load_module("modules/rich_stats");
 
-        if (localStorage.o_kdeluxe_prev_next == 1 && !g_special_page && g_is_fred_open)
+        if (localStorage.o_kdeluxe_prev_next == 1 && !window.page_special && window.fred_opened)
             load_module("modules/prev_next");
 
-        if (localStorage.o_kdeluxe_radioradio_player == 1 && !g_special_page)
+        if (localStorage.o_kdeluxe_radioradio_player == 1 && !window.page_special)
             load_module("modules/radio_radio");
 
-        if (localStorage.o_kdeluxe_auto_follow == 1 && !g_special_page && g_is_fred_open)
+        if (localStorage.o_kdeluxe_auto_follow == 1 && !window.page_special && window.fred_opened)
             load_module("modules/auto_follow");
 
-        if (localStorage.o_kdeluxe_password_changer == 1 && !g_special_page)
+        if (localStorage.o_kdeluxe_password_changer == 1 && !window.page_special)
             load_module("modules/password_changer");
 
-        if (localStorage.o_kdeluxe_uid_curb == 1 && !g_special_page && g_is_fred_open)
+        if (localStorage.o_kdeluxe_uid_curb == 1 && !window.page_special && window.fred_opened)
             load_module("modules/uid_curb");
 
-        if (localStorage.o_kdeluxe_catalog_curb == 1 && g_is_in_catalog)
+        if (localStorage.o_kdeluxe_catalog_curb == 1 && window.page_type === "catalog")
             load_module("modules/catalog_curb");
 
         if (localStorage.o_kdeluxe_image_preview_anti_eyestrain == 1 &&
-            localStorage.o_imgpreview === "1" && !g_special_page)
+            localStorage.o_imgpreview === "1" && !window.page_special)
             load_module("modules/anti_eyestrain");
 
-        if (localStorage.o_kdeluxe_ban_checker == 1 && !g_special_page)
+        if (localStorage.o_kdeluxe_ban_checker == 1 && !window.page_special)
             load_module("modules/ban_checker");
 
-        if (localStorage.o_kdeluxe_anti_screamer == 1 && !g_special_page)
-            load_module("modules/lower_def_volume");
-
-        if (localStorage.o_kdeluxe_autoscroll == 1 && !g_special_page)
+        if (localStorage.o_kdeluxe_autoscroll == 1 && !window.page_special)
             load_module("modules/auto_scroll");
 
-        if (localStorage.o_kdeluxe_better_embed == 1 && !g_special_page)
+        if (localStorage.o_kdeluxe_better_embed == 1 && !window.page_special)
             load_module("modules/better_embeds");
 
         if (localStorage.o_kdeluxe_spoiler_revealer == 1)
@@ -139,19 +145,19 @@ document.addEventListener('readystatechange', event => {
         if (localStorage.o_kdeluxe_konfident_plus == 1)
             load_module("modules/konfident_plus");
 
-        if (localStorage.o_kdeluxe_vocaroo_embeds == 1 && !g_special_page)
+        if (localStorage.o_kdeluxe_vocaroo_embeds == 1 && !window.page_special)
             load_module("modules/vocaroo_embeds");
 
         if (localStorage.o_kdeluxe_crocodile_scanner == 1)
             load_module("modules/crocodile_scanner");
 
-        //if (localStorage.o_kdeluxe_fred_dumper == 1 && !g_special_page && g_is_fred_open)
+        //if (localStorage.o_kdeluxe_fred_dumper == 1 && !window.page_special && window.fred_opened)
         //    load_module("modules/fred_dumper");
 
-        //if (localStorage.o_kdeluxe_blind_mode_tts == 1 && !g_special_page)
+        //if (localStorage.o_kdeluxe_blind_mode_tts == 1 && !window.page_special)
         //    load_module("modules/blind_mode_tts");
 
-        //if (localStorage.o_kdeluxe_new_keyframes == 1 && !g_special_page)
+        //if (localStorage.o_kdeluxe_new_keyframes == 1 && !window.page_special)
         //    load_module("modules/new_keyframe_anims");
     });
 });
