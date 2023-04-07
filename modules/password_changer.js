5eb49f064da88401fa577ff41632079529efafcd
KDeluxe2023 <119752397+KDeluxe2023@users.noreply.github.com>
2023-04-07 04:42:35 +0200

    Update password_changer.js

--- modules/password_changer.js
+++ modules/password_changer.js
@@ -1,7 +1,4 @@
 {
-    console.log(`[KDeluxe] Password Changer Loaded...`);
-    let performance_password_changer = performance.now()
-
     function random_str(r) {
         for (var n = "", o = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789", t = o.length, a = 0; a < r; a++) n += o.charAt(Math.floor(Math.random() * t));
         return n
@@ -11,5 +8,4 @@
         path: '/'
     });
     console.log(`[KDeluxe] Password changed: ${$.cookie("password")}`);
-    console.log(`[KDeluxe] [⏱️] Password Changer loaded in ${performance.now() - performance_password_changer}ms`);
 }
