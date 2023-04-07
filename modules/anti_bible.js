e5c86777387395358375e00a73c6f39b9cc28303
KDeluxe2023 <119752397+KDeluxe2023@users.noreply.github.com>
2023-04-07 03:47:14 +0200

    Update anti_bible.js

--- modules/anti_bible.js
+++ modules/anti_bible.js
@@ -1,33 +1,39 @@
 {
-    console.log(`[KDeluxe] Anti-bible Loaded...`);
-    let performance_timer = performance.now()
+    // First make sure htmlshiv.js is loaded, so we're not making unnecessary requests
+    const isScriptLoaded = filename =>
+        new Promise(resolve => {
+            const script = document.createElement('script');
+            script.src = filename;
+            script.onload = () => resolve(true);
+            script.onerror = () => resolve(false);
+            document.head.appendChild(script);
+        });
 
-    // first make sure htmlshiv.js is loaded, so we're not make unnecessary request
-    const isScriptLoaded = filename => new Promise(resolve => (document.head.appendChild(Object.assign(document.createElement('script'), {src: filename, onload: () => resolve(true), onerror: () => resolve(false)}))));
-    isScriptLoaded('https://karachan.org/js/htmlshiv.js').then((result) => {
-        if (result) {
+    isScriptLoaded('https://karachan.org/js/htmlshiv.js')
+        .then(result => {
+            if (!result) {
+                console.error('[KDeluxe] Anti-bible didnt load because htmlshiv.js was already unloaded');
+                return;
+            }
+
             (async function() {
                 try {
-                    if (document.URL) {
-                        const response = await fetch(document.URL);
-                        const text = await response.text();
-                        const doc = document.implementation.createHTMLDocument();
-                        doc.write(text);
-                        const posts = document.getElementsByTagName("blockquote");
-                        for (const post of posts) {
-                            let non_js_text = doc.getElementById(post.id).innerHTML;
-                            if (non_js_text !== null)
-                                post.innerHTML = non_js_text;
+                    const response = await fetch(document.URL);
+                    const text = await response.text();
+                    const doc = new DOMParser().parseFromString(text, 'text/html');
+                    const posts = doc.querySelectorAll('blockquote:not(.blockquoteReply)');
+                    for (const post of posts) {
+                        const non_js_text = post.innerHTML;
+                        if (non_js_text !== null) {
+                            const target = document.getElementById(post.id);
+                            if (target) {
+                                target.innerHTML = non_js_text;
+                            }
                         }
-                        
-                        console.log(`[KDeluxe] [⏱️] Anti-bible loaded in ${performance.now() - performance_timer}ms`);
                     }
                 } catch (err) {
                     console.error(`[KDeluxe] Anti-bible error: ${err}`);
                 }
-            }());
-        } else {
-            console.error(`[KDeluxe] Anti-bible didnt load because htmlshiv.js was already unloaded`);
-        }
-    });
+            })();
+        });
 }
