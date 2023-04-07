df5139fa7366b67168cd2ee6559b11af1475ea37
KDeluxe2023 <119752397+KDeluxe2023@users.noreply.github.com>
2023-04-07 04:42:09 +0200

    Delete lower_def_volume.js

--- modules/lower_def_volume.js
+++
@@ -1,9 +0,0 @@
-{
-    console.log(`[KDeluxe] Lower Default Volume Loaded...`);
-    let performance_lower_def_volume = performance.now()
-
-    let volume = 0.1
-    $("#player").prop("volume", volume);
-
-    console.log(`[KDeluxe] [⏱️] Lower Default Volume loaded in ${performance.now() - performance_lower_def_volume}ms`);
-}
