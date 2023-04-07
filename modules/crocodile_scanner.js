f393b3a16ce63678f674c7da8e9bb0518d202fbb
KDeluxe2023 <119752397+KDeluxe2023@users.noreply.github.com>
2023-04-07 04:40:18 +0200

    Update crocodile_scanner.js

--- modules/crocodile_scanner.js
+++ modules/crocodile_scanner.js
@@ -1,11 +1,7 @@
 {
-    console.log(`[KDeluxe] Crocodile scanner  Loaded...`);
-    let performance_autofollow = performance.now()
-
+    // TO-DO: dodać wykrywanie innych rodzajów krokodyli (nie tylko TVP)
     const links = document.querySelectorAll("a[href*='tvp.info/13977779']");
     links.forEach(link => {
         link.textContent = link.textContent.replace("tvp.info/13977779", "🐊");
     });
-
-    console.log(`[KDeluxe] [⏱️] Crocodile scanner loaded in ${performance.now() - performance_autofollow}ms`);
 }
