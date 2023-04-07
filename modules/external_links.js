4ab30c1cb81aced61a21f6f501f90e2c69826d55
KDeluxe2023 <119752397+KDeluxe2023@users.noreply.github.com>
2023-04-07 04:40:54 +0200

    Update external_links.js

--- modules/external_links.js
+++ modules/external_links.js
@@ -1,13 +1,8 @@
 {
-    console.log(`[KDeluxe] External Links Loaded...`);
-    let performance_external_links = performance.now()
-
     var links = document.getElementsByClassName('postlink');
     for (var i = 0; i < links.length; i++) {
         links[i].setAttribute('href', links[i].getAttribute('href').replace('https://href.li/?', ''));
         links[i].setAttribute('target', '_blank');
         links[i].setAttribute('rel', 'noreferrer');
     }
-
-    console.log(`[KDeluxe] [⏱️] External Links loaded in ${performance.now() - performance_external_links}ms`);
 }
