78d8db6ca6c8c4e2f15484757a3a1571c4fde5a9
KDeluxe2023 <119752397+KDeluxe2023@users.noreply.github.com>
2023-04-07 04:39:28 +0200

    Update clickable_boardname.js

--- modules/clickable_boardname.js
+++ modules/clickable_boardname.js
@@ -1,13 +1,8 @@
 {
-    console.log(`[KDeluxe] Clickable Board name Loaded...`);
-    let performance_timer = performance.now();
-
     let boardTitle = document.querySelector('.boardBanner'); //boardTitle
     boardTitle.addEventListener('click', function() {
         //window.location.reload();
         let board = document.querySelector('input[name="board"]').value;
         window.location.href = '/' + board + '/';
     });
-
-    console.log(`[KDeluxe] [⏱️] Clickable Board name loaded in ${performance.now() - performance_timer}ms`);
 }
