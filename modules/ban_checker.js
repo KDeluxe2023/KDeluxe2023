81fef4da7b16539aae12e5eb61a8175426dfcca1
KDeluxe2023 <119752397+KDeluxe2023@users.noreply.github.com>
2023-04-07 04:36:06 +0200

    Update ban_checker.js

--- modules/ban_checker.js
+++ modules/ban_checker.js
@@ -1,31 +1,44 @@
 {
-    console.log(`[KDeluxe] Ban Checker Loaded...`);
-    let performance_timer = performance.now()
-
-    // create red info label tellin you that you're banned
     const postform = document.querySelector('#postform');
-    const banned = document.createElement('h1');
+    const banned = document.createElement('h3');
     banned.id = 'banned';
     postform.insertAdjacentHTML('afterend', banned.outerHTML);
 
-    // send http request to banned.php
     function check_for_ban() {
-        var xhr = new XMLHttpRequest();
-        xhr.open("GET", "https://karachan.org/banned.php");
-        xhr.onload = function() {
-            if (!this.responseText.includes("NOT BANNED")) {
-                var bannedElement = document.getElementById("banned");
-                bannedElement.textContent = "Jesteś zbanowany";
-                bannedElement.style.textAlign = "center";
-                bannedElement.style.color = "red";
-            }
-        };
-        xhr.send();
+        fetch("https://karachan.org/banned.php")
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error("Network response was not ok");
+                }
+                return response.text();
+            })
+            .then(text => {
+                if (!text.includes("NOT BANNED")) {
+                    const bannedElement = document.getElementById("banned");
+
+                    // extract third paragraph element's text from fetched document
+                    const remoteDoc = new DOMParser().parseFromString(text, "text/html");
+                    const boxcontentDiv = remoteDoc.querySelector(".boxcontent");
+                    const paragraphs = boxcontentDiv.querySelectorAll("p");
+                    const thirdParagraph = paragraphs[3];
+                    const thirdParagraphText = thirdParagraph.textContent;
+
+                    bannedElement.addEventListener("click", () => {
+                        window.location.href = "https://karachan.org/banned.php";
+                    });
+
+                    // set bannedElement.textContent to third paragraph's text
+                    bannedElement.textContent = thirdParagraphText;
+                    bannedElement.style.textAlign = "center";
+                    bannedElement.style.color = "red";
+                }
+            })
+            .catch(error => {
+                console.error("[KDeluxe] Error fetching banned status:", error);
+            });
 
         setTimeout(check_for_ban, 1000 * 10);
     }
 
     check_for_ban();
-
-    console.log(`[KDeluxe] [⏱️] Ban Checker loaded in ${performance.now() - performance_timer}ms`);
 }
