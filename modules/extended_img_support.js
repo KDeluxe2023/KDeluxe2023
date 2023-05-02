4a53abcff103499062165c6b5e1e37aa18021ea5
KDeluxe2023 <119752397+KDeluxe2023@users.noreply.github.com>
2023-05-02 20:13:02 +0800

    Update extended_img_support.js

--- modules/extended_img_support.js
+++ modules/extended_img_support.js
@@ -1,35 +1,54 @@
 {
     const file_input = document.querySelectorAll('input[type=file]');
 
-    function convertToJPEG(file) {
+
+    // Function to convert webp/avif to jpeg
+    function convertToJPEG(fileInput) {
+        const file = fileInput.files.item(0)
+
+        // Check if file is valid
         if (!file || !['image/avif', 'image/webp'].includes(file.type)) {
-            return;
+            return
         }
 
-        const canvas = document.createElement("canvas");
-        const context = canvas.getContext("2d");
-        const imageUrl = URL.createObjectURL(file);
-        const image = new Image();
+        // Create canvas and context
+        const canvas = document.createElement("canvas")
+        const context = canvas.getContext("2d")
 
-        image.src = imageUrl;
+        // Create image element and load the file
+        const imageUrl = URL.createObjectURL(file)
+        const image = new Image()
+        image.src = imageUrl
+
+        // Once the image is loaded, draw it onto the canvas and convert to jpeg blob
         image.onload = () => {
-            canvas.width = image.width;
-            canvas.height = image.height;
-            context.drawImage(image, 0, 0);
+            canvas.width = image.width
+            canvas.height = image.height
+            context.drawImage(image, 0, 0)
             canvas.toBlob((blob) => {
-                const dataTransfer = new DataTransfer();
+                const dataTransfer = new DataTransfer()
                 const jpegFile = new File([blob], file.name, {
                     type: "image/jpeg"
-                });
-                dataTransfer.items.add(jpegFile);
-                file.parentNode.replaceChild(file.cloneNode(true), file);
-                file.files = dataTransfer.files;
-                URL.revokeObjectURL(imageUrl);
-                canvas.remove();
-            }, 'image/jpeg');
+                })
+                dataTransfer.items.add(jpegFile)
+
+                // Replace the original file input with a new one containing the jpeg file
+                const newFileInput = fileInput.cloneNode(true)
+                newFileInput.files = dataTransfer.files
+                fileInput.parentNode.replaceChild(newFileInput, fileInput)
+
+                URL.revokeObjectURL(imageUrl)
+                canvas.remove()
+            }, 'image/jpeg')
         }
     }
 
-    if (file_input.length)
-        file_input.forEach(input => input.addEventListener("change", () => convertToJPEG(input.files.item(0))));
+    // Add event listener to each input file element
+    if (file_input.length) {
+        file_input.forEach(input => {
+            input.addEventListener("change", () => {
+                convertToJPEG(input)
+            })
+        });
+    }
 }
